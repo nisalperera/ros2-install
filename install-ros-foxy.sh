ed5cd6199416498d679e12b2a3ac924d415eed02
Nisal Chinthana Perera Gallara Acharige <chinthanapereranisal@gmail.com>
2025-10-17 10:30:22 +0530

    bug fixes

--- install-ros-foxy.sh
+++ install-ros-foxy.sh
@@ -33,8 +33,7 @@ echo "source /opt/ros/foxy/setup.bash" >> $HOME/.bashrc
 # install Ignition Citadel
 sudo sh -c 'echo "deb https://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 curl https://packages.osrfoundation.org/gazebo.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install ignition-citadel
+sudo apt-get update && sudo apt-get install ignition-citadel -y
 
 # install xacro and joint state publisher GUI ROS packages
 sudo apt install ros-foxy-gazebo-ros-pkgs ros-foxy-xacro ros-foxy-joint-state-publisher-gui -y
